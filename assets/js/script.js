cc77b2639e6b5ae655bde276a8fd96ba8f06306d
luckylemontree <mailwcq@gmail.com>
2026-05-26 19:54:37 +0100

    Add chechAnswer function to submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,10 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type").toLowerCase();
-                
+
                 runGame(gameType);
             }
             //let gameType = this.getAttribute("data-type");
@@ -31,18 +31,44 @@ function runGame(gameType) {
 
     //for addition button been clicked
     if (gameType === "addition") {
-       displayAdditonQuestion(num1, num1);
+        displayAdditonQuestion(num1, num1);
     } else {
         alert(`unkown game type:${gameType}`);
         throw ` unkown game type:${gameType}. Aborting `;
     }
 }
-
+/**
+ * Checks the answer agaist the first elemet in
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect){
+        alert("Hey,you got it right!:D");
+    }else{
+        alert(`Awwwwwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
 
 }
-
+/**
+ * Gets the operands(the numbers) and the operator(plus,minus etc.)
+ * directly from the DOM,and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    //Caculate addition
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`unkown game type:${gameType}`);
+        throw ` unkown game type:${gameType}. Aborting `;
+    }
 
 }
 
